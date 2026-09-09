1d1ab0779ad4967a1381f7041635e7c47437f69e
yohaneskunfu-cell <yohaneskunfu@gmail.com>
2026-09-09 21:00:48 +0700

    Add files via upload

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: { rejectUnauthorized: false }
+});
+
+export default pool;
